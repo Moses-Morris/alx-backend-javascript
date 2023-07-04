cb682219c93fef2cc083bfefc283095e16f3955e
Moses-Morris <mosesmorrisdev@gmail.com>
2023-07-04 14:29:35 +0000

    A set with certain strings.

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+function cleanSet(set, startSting) {
+  const values = [];
+
+  if (!startSting || startSting.length === 0) {
+    return values.join('-');
+  }
+
+  for (const element of set) {
+    if (element && element.startsWith(startSting)) {
+      values.push(element.slice(startSting.length));
+    }
+  }
+  return values.join('-');
+}
+export default cleanSet;
