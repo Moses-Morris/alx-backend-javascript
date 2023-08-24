b58aeca4ad8d64580a97c0eedcb57a01dda75643
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-24 09:20:27 +0000

    Basic test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
 const calculateNumber = require('./0-calcul');
-const assert = require('chai').assert;
+const assert = require('assert');
 
 describe('calculateNumber', function() {
   it('should return the correct sum when both numbers are integers', function() {
