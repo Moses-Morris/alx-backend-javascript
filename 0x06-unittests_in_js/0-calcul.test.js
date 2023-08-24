4c791ff156c949d7e55b9ccd1f1937e02bac8a19
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-24 08:02:25 +0000

    Multiple tests to the maths function.

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,4 +6,24 @@ describe('calculateNumber', function() {
     const result = calculateNumber(3, 5);
     assert.strictEqual(result, 8);
   });
+
+  it('should round input numbers and return the correct sum', function() {
+    const result = calculateNumber(3.4, 5.6);
+    assert.strictEqual(result, 9);
+  });
+
+  it('should handle negative input numbers', function() {
+    const result = calculateNumber(-3.6, -5.3);
+    assert.strictEqual(result, -9);
+  });
+
+  it('should handle one rounded input number', function() {
+    const result = calculateNumber(3.6, 5);
+    assert.strictEqual(result, 9);
+  });
+
+  it('should return 0 if both input numbers are 0', function() {
+    const result = calculateNumber(0, 0);
+    assert.strictEqual(result, 0);
+  });
 });
