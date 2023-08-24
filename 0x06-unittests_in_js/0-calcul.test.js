e88bb4848464df0e9eb3b30f387f185e06ff61c2
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-24 07:48:55 +0000

    Delet file.

--- 0x06-unittests_in_js/0-calcul.test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('chai').assert;
-const calculateNumber = require('./0-calcul');
-
-describe('calculateNumber', () => {
-	it('Should return sum of rounded numbers:', () => {
-		let result = calculateNumber(1,3);
-		assert.strictEqual(result, 4);
-	});
-});
