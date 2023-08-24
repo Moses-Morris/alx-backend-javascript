8648dcd69b702e71bdd454cefc6b6f28175272de
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-24 14:45:03 +0000

    Grapes.

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,41 +1,33 @@
-const express = require("express");
+const express = require('express');
 
-const app = express()
-const port = 7865
+const app = express();
+const port = 7865;
 
 app.use(express.json());
 
-app.param('id', (req, res, next, id) => {
-  if (!isValidId(id)) {
-    return res.status(400).send('Invalid ID');
-  }
-  next();
+app.get('/', (req, res) => {
+  res.end('Welcome to the payment system');
 });
 
-app.get("/", (req, res) => {
-    res.send("Welcome to the paymnet system");
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.end(`Payment methods for cart ${req.params.id}`);
 });
 
-app.get("/cart/:id", (req, res) => {
-    const id = req.params.id;
-    return res.send(`Payment methods for cart ${id}`)
+app.get('/available_payments', (req, res) => {
+  const obj = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    },
+  };
+  res.json(obj);
 });
 
-app.get("/available_payments", (req, res) => {
-    res.json({payment_methods: {credit_cards: true, paypal: false}})
+app.post('/login', (req, res) => {
+  const username = req.body.userName;
+  res.end(`Welcome ${username}`);
 });
-
-app.get("/login", (req, res) => {
-    const userName = req.body.userName;
-    res.send(`Welcome ${userName}`);
-});
-
-function isValidId(id) {
-  return /^\d+$/.test(id);
-}
 
 app.listen(port, () => {
-  console.log("API available on localhost port 7865");
+  console.log('API available on localhost port 7865');
 });
-
-module.exports = app;
