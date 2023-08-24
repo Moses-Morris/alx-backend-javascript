5b1489c87022eff2c052c7b93871093195b98422
Moses-Morris <mosesmorrisdev@gmail.com>
2023-08-24 13:38:52 +0000

    Create a testing for basic app. Use the request module to get HTTP responses.

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,10 +1,10 @@
 const express = require("express");
 
 const app = express()
-const port = 7865
+const port = 7865;
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the paymnet system");
+    res.send("Welcome to the payment system");
 });
 
 app.listen(port, () => {
